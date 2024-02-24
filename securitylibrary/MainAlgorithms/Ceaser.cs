7a25a10dc20c73c79677a448ff59948bb6862a9a
Kareem adel <pi.kareem14@gmail.com>
2024-02-25 01:16:11 +0200

    Adding Analyse function

--- securitylibrary/MainAlgorithms/Ceaser.cs
+++ securitylibrary/MainAlgorithms/Ceaser.cs
@@ -41,7 +41,8 @@ namespace SecurityLibrary
             //char[] alphabetarray = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
             plainText = plainText.ToLower();
             cipherText = cipherText.ToLower();
-            int mainkey=Array.IndexOf(alphabetarray, cipherText[0]) - Array.IndexOf(alphabetarray, plainText[0]);
+            int Temp=(Array.IndexOf(alphabetarray, cipherText[0]) - Array.IndexOf(alphabetarray, plainText[0]));
+            int mainkey= Temp >= 0? Temp : Temp + 26;
             return mainkey;
         }
     }
